b6e6950270b99b155035442cdafc104c636b4d7f
CreponneKarim <jk_laouchedi@esi.dz>
2023-12-21 10:38:55 +0100

    asdasd

--- init.sh
+++ init.sh
@@ -5,3 +5,10 @@ sudo apt install build-essential
 sudo apt install libsdl2-dev 
 sudo apt install libsdl2-ttf-dev
 sudo apt-get install libglfw3
+sudo apt install gh
+gh repo clone ocornut/imgui
+
+sed -i 's/SDL.h/SDL2\/SDL.h/g' ./imgui/backends/imgui_impl_sdlrenderer2.cpp
+sed -i 's/SDL.h/SDL2\/SDL.h/g' ./imgui/backends/imgui_impl_sdl2.cpp
+sed -i 's/SDL_syswm.h/SDL2\/SDL_syswm.h/g' ./imgui/backends/imgui_impl_sdlrenderer2.cpp
+sed -i 's/SDL_syswm.h/SDL2\/SDL_syswm.h/g' ./imgui/backends/imgui_impl_sdl2.cpp
